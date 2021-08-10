2cc6ef0e732fc40efdd8b16a18cc2ae14d7cffa7
Tales Araujo <tales.araujo@zup.com.br>
2021-08-10 14:22:45 -0300

    usando form com post

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -10,7 +10,7 @@ import java.io.PrintWriter;
 public class NovaEmpresaServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         System.out.println("Cadastrando nova empresa!");
         PrintWriter out = resp.getWriter();
