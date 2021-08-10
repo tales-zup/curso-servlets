d699b498aa65be30e70447139422cbec2e5ea886
Tales Araujo <tales.araujo@zup.com.br>
2021-08-10 14:00:02 -0300

    novo servlet recebendo parametros

---
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,25 @@
+package br.com.alura.gerenciador.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        System.out.println("Cadastrando nova empresa!");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("Parabéns, uma nova empresa foi criada!\n");
+        out.println(req.getParameter("nome"));
+        out.println("</body>");
+        out.println("</html>");
+
+    }
+}
